a4a689e34490ab27abb3cce6ca5281931a455086
UDONTO <florian.kiel@gmx.net>
2025-05-13 10:14:44 +0200

    feat: add GitHub Actions workflow for deploying Next.js site to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,36 @@
-import createMDX from '@next/mdx'
- 
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/out/nextjs-github-pages",
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-}
- 
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
- 
+});
+
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
