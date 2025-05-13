c3484eac80e5e87b181d58bc833e46e0c2fe90e5
UDONTO <florian.kiel@gmx.net>
2025-05-13 11:41:52 +0200

    refactor: clean up next.config.mjs by removing unnecessary comments and fixing images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 import createMDX from "@next/mdx";
 
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   /**
    * Enable static exports.
@@ -10,6 +8,12 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
   output: "export",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/floriankiel-portfolio-heroui",
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
@@ -20,7 +24,7 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
    */
   images: {
-      : true,
+    unoptimized: true,
   },
 };
 
